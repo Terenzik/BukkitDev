5371ef4e99d966aca83ad3fd604d1c0b733ed720
Michael Smith <terenzik@gmail.com>
2015-06-01 19:37:26 +1200

    Update to the master
    Slight Changes

---
+++ src/me/Terenzik/BukkitDev/Commands/Help.java
@@ -0,0 +1,16 @@
+package me.Terenzik.BukkitDev.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Michael on 1/06/15.
+ */
+public class Help implements JavaPlugin {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return super.onCommand(sender, cmd, label, args);
+    }
+}
